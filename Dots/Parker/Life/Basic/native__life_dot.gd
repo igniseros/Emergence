809f751b6b86ace9aa8c91cc2cd3ec614e40b826
igniseros <42523737+igniseros@users.noreply.github.com>
2022-01-30 17:51:07 -0500

    starting on life

---
+++ Dots/Parker/Life/Basic/native__life_dot.gd
@@ -0,0 +1,15 @@
+extends LifeDot
+class_name NativeLifeDot
+
+func _init():
+	#set name
+	name = "Native Life"
+	#set clors
+	color_one = Color(1,1,1)
+	color_two = Color(1,1,1)
+	color_three = Color(0,1,0)
+	
+	energy = 5
+
+func tick():
+	pass
